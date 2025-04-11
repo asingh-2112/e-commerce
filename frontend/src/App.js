7bb7bf4d2177945f4f0ded4c3578547e9a7364be
asingh-2112 <aksinghrbj2112@gmail.com>
2025-04-12 03:24:45 +0530

    added carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
+import HomePage from './customer/pages/home/HomePage';
 
 function App() {
   return (
     <div className="">
       <Navigation/>
     <div>
-      Home
+      <HomePage/>
     </div>
     </div>
   );
---
+++ frontend/src/customer/components/homeCarousel/MainCarouselData.js
@@ -0,0 +1,18 @@
+export const mainCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/9/19_mob_1.jpg",
+        path:"/women/clothing/womens_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/0/20_mob.jpg",
+        path:"/women/clothing/womens_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/8/18_mob_1.jpg",
+        path:"/women/clothing/womens_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/1/21_mob.jpg",
+        path:"/women/clothing/womens_dress"
+    },
+]
